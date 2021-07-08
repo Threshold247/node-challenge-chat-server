58ed4995a2f41c836b697071c5e185c7670a0d5b
Threshold247 <65241733+Threshold247@users.noreply.github.com>
2021-07-08 12:03:32 +0200

    updated Routes

--- server.js
+++ server.js
@@ -1,24 +1,12 @@
 const express = require("express");
 const cors = require("cors");
-
+const messages = require("./messages.json")
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
-
-const welcomeMessage = {
-  id: 0,
-  from: "Bart",
-  text: "Welcome to CYF chat system!",
-  };
-
-
-//This array is our "data store".
-//We will start with one message in the array.
-//Note: messages will be lost when Glitch restarts our server.
-const messages = [welcomeMessage];
 
 app.get("/", function (request, response) {
   response.sendFile(__dirname + "/index.html");
@@ -27,6 +15,17 @@ app.get("/", function (request, response) {
 //read all messages
 app.get("/messages", (request,response) => {
   response.json(messages);
+});
+
+//search by a query term
+function search(word) {
+  return messages.filter(el => el.text.toLowerCase().includes(word.toLowerCase()));
+}
+
+app.get("/messages/search", function(request, response) {
+  const searchWord = request.query.word;
+  const result = search(searchWord);
+  response.send(result);
 });
 
 //read one message by Id
@@ -41,17 +40,34 @@ app.get("/messages/:id", (request,response) => {
     response.status(400).send(`Id: ${id} not found`)
   };
 });
-//Create a messages
-app.post("/", (request,response) => {
+
+
+//Create a message
+app.post("/messages", (request,response) => {
   const newMessage = {
     id: request.body.id,
     from: request.body.from,
     text: request.body.text
   };
-  messages.push(newMessage)
-  response.json(messages)
+  messages.push(newMessage);
+  response.json(messages);
 
-})
+});
+
+//delete a message
+app.delete("/messages/:id", (request,response) => {
+  const id = request.params.id;
+  console.log(id)
+
+  const match = messages.some(el => el.id === parseInt(id) );
+  console.log(match);
+  if(match){
+    response.json({msg: `Id:${id} deleted`})
+  } else {
+  } response.status(400).send("message not found")
+
+});
+
 
 
 app.listen(process.env.PORT || 5000, function () {
