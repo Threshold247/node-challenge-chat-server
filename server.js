cd69eac764851a1e12655ebbfa037c85d41ef0af
Threshold247 <65241733+Threshold247@users.noreply.github.com>
2021-07-09 11:54:27 +0200

    Added update message values

--- server.js
+++ server.js
@@ -18,8 +18,8 @@ app.get("/messages", (request,response) => {
 });
 
 //search by a query term
-function search(word) {
-  return messages.filter(el => el.text.toLowerCase().includes(word.toLowerCase()));
+function search(text) {
+  return messages.filter(el => el.text.toLowerCase().includes(text.toLowerCase()));
 }
 
 app.get("/messages/search", function(request, response) {
@@ -46,12 +46,33 @@ app.get("/messages/:id", (request,response) => {
 app.post("/messages", (request,response) => {
   const newMessage = {
     id: request.body.id,
-    from: request.body.from,
+    From: request.body.From,
     text: request.body.text
   };
+  if(!newMessage.id || !newMessage.From || !newMessage.text) {
+    response.status(400).send(`Please include either Id, From or text`);
+  }
   messages.push(newMessage);
   response.json(messages);
+});
 
+// update a message
+app.put("/messages/:id", (request,response) => {
+  const {id} = request.params;
+  const match = messages.some(el => el.id === parseInt(id) );
+
+  if(match) {
+    const updateMessage = request.body;
+    messages.forEach(el => {
+      if(el.id === parseInt(id)) {
+        el.From ? updateMessage.From : el.From;
+        el.text ? updateMessage.text : el.text;
+        response.json({msg: "Message updated", el})
+      }
+    });
+   } else {
+    response.status(400).send(`Id: ${id} not found`)
+  };
 });
 
 //delete a message
